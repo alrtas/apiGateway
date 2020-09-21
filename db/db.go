b5b0b86effbed6e1cbf577a9710e338307b4c410
Thiago Alberto da Silva <alrrtas@gmail.com>
2020-09-21 17:05:14 -0300

    Criado modelo de conexão redis

--- db/db.go
+++ db/db.go
@@ -2,10 +2,18 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	//Fica como empty porque só é necessário somente em runtime
 	//Necessário instalação prévia do modulo
 	_ "github.com/go-sql-driver/mysql"
+	//Documentação: https://github.com/go-sql-driver/mysql
+	"github.com/go-redis/redis"
+	_ "github.com/go-redis/redis/v8"
+
+	//Documentação: https://github.com/go-redis/redis
+	_ "github.com/elastic/go-elasticsearch/v7"
+	//Documentação: https://github.com/elastic/go-elasticsearch
 )
 
 //ConectaMySQL função que conecta na  base de dados A
@@ -28,4 +36,11 @@ func ConectaElasticSearch() {
 //ConectaRedis função que conecta na base de dados C
 func ConectaRedis() {
 
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	fmt.Println(rdb)
+	//return rdb
 }
